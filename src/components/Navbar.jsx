3e3b2a4740b678b6b782e7c37b392f35b79fd5cc
nitesh2920 <niteshtiwari0234@gmail.com>
2024-11-05 13:30:53 +0530

    github added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
+import { FaGithub } from "react-icons/fa";
 
 export function Navbar() {
   return (
     <nav className="border-b">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <h1 className="text-xl font-bold">Jarvis Quotes</h1>
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
 
             <ModeToggle/>
+            <a
+            href="https://github.com/nitesh2920"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-800 dark:text-white hover:text-slate-500 transition duration-300"
+          >
+            <FaGithub size={26} />
+          </a>
           <Button variant="ghost">About</Button>
           <Button>Login</Button>
         </div>
